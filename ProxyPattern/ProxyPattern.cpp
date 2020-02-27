df28b3b18e99dffd466beba99ee0d16fa64e26e3
Amit <amit.agrawal1994@gmail.com>
2020-02-27 20:29:59 +0530

    Design Pattern : Proxy pattern

---
+++ ProxyPattern/ProxyPattern.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+1. We can use proxy when we may want to use a class that can perform as an interface to something else.
+2. Proxy is heavily used to implement lazy loading related usecases where we do not want to create full object until it is actually needed.
+3. A proxy can be used to add an additional security layer around the original object as well.
+
+eg: Before actually accessing a website, proxy can check if the syntax/length of passwords/email is correct or not
+*/
+
+// Abstract class
+struct DrivingLicenseAuthority
+{
+    virtual void issueDrivingLicense() = 0;
+};
+
+//concrete class - Actually intented to be accessed by end user
+class HyderabadDrivingAuthority : public DrivingLicenseAuthority
+{
+    public:
+        void issueDrivingLicense()
+        {
+            cout<<"Driving license is being issued by Hyderabad Driving Authority"<<endl;
+        }
+};
+
+//proxy class
+//Proxy class should inherit abstract base class
+class HyderabadProxy : public DrivingLicenseAuthority
+{
+    HyderabadDrivingAuthority* hydAuthority;
+    int driverAge;
+
+    public:
+        HyderabadProxy(int driverAge = 20)
+        {
+            this->driverAge = driverAge;
+            hydAuthority = new HyderabadDrivingAuthority;
+        }
+
+        //This function name and prototype should be same as original
+        void issueDrivingLicense()
+        {
+            if(driverAge > 18)
+            {
+                cout<<"Driver age: "<<driverAge<<" is greater than 18. Generating driver license"<<endl;
+                hydAuthority->issueDrivingLicense();
+            }
+            else
+            {
+                cout<<"Driver age: "<<driverAge<<" is less than 18. Denying driver license"<<endl;
+            }
+        }
+};
+
+int main()
+{
+    DrivingLicenseAuthority* authority = new HyderabadProxy(15);
+    authority->issueDrivingLicense(); // actual call by end user
+
+    authority = new HyderabadProxy(26);
+    authority->issueDrivingLicense();
+
+    return 0;
+}
